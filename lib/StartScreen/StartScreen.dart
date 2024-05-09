a08cdb7e168e92eaedf0a786623e12b92b81360e
RAWAN <@kahwajirawan501>
2024-05-09 13:31:32 +0300

    اضفت

--- lib/StartScreen/StartScreen.dart
+++ lib/StartScreen/StartScreen.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 
 class StartScreen extends StatelessWidget {
   const StartScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: AspectRatio(
+        aspectRatio: 16 / 9, // تنسيق الفيديو
+        child: VideoPlayer(
+          VideoPlayerController.asset('assets/video/start_video.mp4'), // استخدام ملف الفيديو من الموارد
+        ),
+      ),
+    );
   }
 }
