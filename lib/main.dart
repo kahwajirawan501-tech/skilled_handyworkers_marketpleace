fc799cb30675a35fd56128abb8e543a809ac796b
RAWAN <@kahwajirawan501>
2024-08-05 16:25:34 +0300

    اضفت

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,28 @@
+import 'dart:typed_data';
+
+import 'package:dio/dio.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:intl/date_symbol_data_local.dart';
 import 'package:skilled_handyworkers_marketpleace/AddPosting/cubit/cubit.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:skilled_handyworkers_marketpleace/ButtonNavigation/BottonNavigationBar.dart';
+import 'package:skilled_handyworkers_marketpleace/ButtonNavigation/cubit/cubit.dart';
+import 'package:skilled_handyworkers_marketpleace/Chat_Messages/cubit/cubit.dart';
+import 'package:skilled_handyworkers_marketpleace/Commint/cubit/cubit.dart';
+import 'package:skilled_handyworkers_marketpleace/EditPost/cubit/cubit.dart';
+import 'package:skilled_handyworkers_marketpleace/Chat_Messages/Message.dart';
+import 'package:skilled_handyworkers_marketpleace/Posting/cubit/cubit.dart';
 import 'package:skilled_handyworkers_marketpleace/Registration/cubitSignUp/cubit.dart';
+import 'package:skilled_handyworkers_marketpleace/SearchModel/cubitLocation/cubit.dart';
+import 'package:skilled_handyworkers_marketpleace/SearchModel/cubitService/cubit.dart';
+import 'package:skilled_handyworkers_marketpleace/SearchScreen/cubit/cubit.dart';
 import 'package:skilled_handyworkers_marketpleace/profileScreens/Setting/cubit/cubit.dart';
+import 'package:skilled_handyworkers_marketpleace/profileScreens/cubit/cubit.dart';
 import 'package:skilled_handyworkers_marketpleace/profileScreens/profileScreen.dart';
 import 'package:skilled_handyworkers_marketpleace/profileScreens/skill/cubit/cubit.dart';
+import 'package:skilled_handyworkers_marketpleace/shared/components/constant.dart';
 import 'package:skilled_handyworkers_marketpleace/shared/network/local/cache_helper.dart';
 import 'package:skilled_handyworkers_marketpleace/shared/network/local/local.dart';
 import 'package:skilled_handyworkers_marketpleace/shared/network/local/local_controller.dart';
@@ -16,13 +32,50 @@ import 'Registration/LoginScreen.dart';
 import 'Registration/cubitConfirmSignUp/cubit.dart';
 import 'Registration/cubitLogin/cubit.dart';
 import 'StartScreen/StartScreen.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:permission_handler/permission_handler.dart';
+
 
 Future<void> main() async {
+
   WidgetsFlutterBinding.ensureInitialized();
+  await initializeNotifications();
+  checkAndRequestPermissions();
+  await initializeDateFormatting('en_US', null);
   DioHelper.init();
   await CacheHelper.init();
   Get.lazyPut<MyLocalController>(() => MyLocalController());
   runApp(const MyApp());
+
+}
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+
+Future<void> initializeNotifications() async {
+  const AndroidInitializationSettings initializationSettingsAndroid =
+  AndroidInitializationSettings('@mipmap/ic_launcher');
+  final InitializationSettings initializationSettings =
+  InitializationSettings(android: initializationSettingsAndroid);
+  await flutterLocalNotificationsPlugin.initialize(initializationSettings);
+
+  const AndroidNotificationChannel channel = AndroidNotificationChannel(
+    'unique_channel_id', // معرف القناة الفريد
+    'Message Notifications',
+    importance: Importance.high,
+    playSound: true,
+  );
+
+  await flutterLocalNotificationsPlugin
+      .resolvePlatformSpecificImplementation<AndroidFlutterLocalNotificationsPlugin>()
+      ?.createNotificationChannel(channel);
+  debugPrint('Notification channel created successfully');
+}
+
+Future<void> checkAndRequestPermissions() async {
+  PermissionStatus status = await Permission.notification.status;
+  if (status.isDenied) {
+    status = await Permission.notification.request();
+  }
+  debugPrint('Notification permission status: ${status.toString()}');
 }
 
 class MyApp extends StatelessWidget {
@@ -36,10 +89,65 @@ class MyApp extends StatelessWidget {
     return MultiBlocProvider(
       providers: [
         BlocProvider(
-          create: (context) => SkillCubit(),
+
+          create:(context) =>SkillCubit(),
+
+
+        ),
+
+        BlocProvider(
+
+          create:(context) =>ProfileCubit()..getProfileInformation(),
+
+
         ),
         BlocProvider(
-          create: (context) => SettingCubit(),
+
+          create:(context) =>SettingCubit(),
+
+
+        ),
+        BlocProvider(
+
+          create:(context) =>HomeCubit(),
+
+
+        ),
+        BlocProvider(
+
+          create:(context) =>ServiceCubit()..getService(),
+
+
+        ),
+        BlocProvider(
+
+          create:(context) =>LocationCubit()..getLocation(),
+
+
+        ),
+        BlocProvider(
+
+          create:(context) =>CubitSearch(),
+
+
+        ),
+        BlocProvider(
+
+          create:(context) =>CommitCubit(),
+
+
+        ),
+        BlocProvider(
+
+          create:(context) =>CubitYourPost(),
+
+
+        ),
+        BlocProvider(
+
+          create:(context) =>AddPostCubit(),
+
+
         ),
         BlocProvider(
           create: (context) => LoginCubit(),
@@ -55,6 +163,18 @@ class MyApp extends StatelessWidget {
           create:(context) =>AddPostCubit(),
 
 
+        ),
+        BlocProvider(
+
+          create:(context) =>EditPostCubit(),
+
+
+        ),
+        BlocProvider(
+
+          create:(context) =>ChatCubit(),
+
+
         )
       ],
       child: GetMaterialApp(
@@ -62,7 +182,7 @@ class MyApp extends StatelessWidget {
         title: 'Skilled handyWorker marketplace System',
         locale: controller.intiallang,
         translations: MyLocal(),
-        home: LoginScreen(),
+        home: BottomNavigationScreen(),
       ),
     );
   }
